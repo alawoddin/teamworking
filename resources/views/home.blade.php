be84bdb73c8cea73e33ea851a0e5cf6fe4d7a9bd
hashmatullah <hashmatullah@gmail.com>
2025-05-24 19:01:50 +0430

    add the h2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,14 @@
 <div>
 
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores alias accusamus impedit labore, commodi tempore aliquam eveniet veniam necessitatibus, est quis illum, et recusandae ducimus repellendus dignissimos natus non incidunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores alias accusamus impedit labore, commodi tempore
+        aliquam eveniet veniam necessitatibus, est quis illum, et recusandae ducimus repellendus dignissimos natus non
+        incidunt.
     </p>
+
+    <h2>
+        this is for demo
+    </h2>
 
 
 </div>
