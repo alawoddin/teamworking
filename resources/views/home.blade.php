8484ee5d8aa38608c337c2f0c6b515922ae99254
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-05-24 18:52:55 +0430

    there a add the p

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <div>
 
-    <h1>hello world</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores alias accusamus impedit labore, commodi tempore aliquam eveniet veniam necessitatibus, est quis illum, et recusandae ducimus repellendus dignissimos natus non incidunt.
+    </p>
+
+
 </div>
